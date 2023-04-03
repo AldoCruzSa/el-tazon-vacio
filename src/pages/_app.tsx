85e9465bc90b0e8ec53a7ae441cadb5e740ddf44
CRUZ SANDOVAL <uie94601@contiwan.com>
2023-04-02 23:36:00 -0600

    vercel analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default MyApp;
